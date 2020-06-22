f2fea251b6a0d456043a044702097959f187a82b
hots <hots@msn.cn>
2020-06-22 10:49:03 +0800

    add http_server

---
+++ http_server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+)
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+type PlayerServer struct {
+	store PlayerStore
+}
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+type StubPlayerStore struct {
+	scores map[string]int
+}
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+} 
