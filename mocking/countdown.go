a5a4393029dc4c9f1e00f069db8a6a150fab4fc1
hots <hots@msn.cn>
2020-06-23 18:42:53 +0800

    bug fixed

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-    "io"
-    "fmt"
-    "os"
-    "time"
+	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
 type Sleeper interface {
-    Sleep()
+	Sleep()
 }
 
 type SpySleeper struct {
-    Calls int
+	Calls int
 }
 
 func (s *SpySleeper) Sleep() {
-    s.Calls++
+	s.Calls++
 }
+
 const finalWord = "Go!"
 const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-    for i := countdownStart; i > 0; i-- {
-        time.Sleep(1 * time.Second)
-        fmt.Fprintln(out, i)
-    }
-    time.Sleep(1 * time.Second)
-    fmt.Fprint(out, finalWord)
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-    Countdown(os.Stdout)
+	Countdown(os.Stdout, &SpySleeper{})
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,24 +1,26 @@
 package main
 
-import "testing"
-import "bytes"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestGreet(t *testing.T) {
-    buffer := &bytes.Buffer{}
-    spySleeper := &SpySleeper{}
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-    Countdown(buffer, spySleeper)
+	Countdown(buffer, spySleeper)
 
-    got := buffer.String() 
-    want := `3
+	got := buffer.String()
+	want := `3
 2
 1
 Go!`
 
-    if got != want {
-        t.Errorf("got '%s' want '%s'", got, want)
-    }
-    if spySleeper.Calls != 4 {
-        t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.calls)
-    }
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	}
 }
